116279fcbe513afd426323428b9a9238698390b5
Clark Brading <scbrading@gmail.com>
2018-06-26 16:18:19 -0400

    Set up some new components for the header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { IdCardComponent } from './id-card/id-card.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IdCardComponent,
+    NavigationComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
