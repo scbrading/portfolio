9cc6890fae707d6ce46560819922fd0ebbdb21fd
Clark Brading <scbrading@gmail.com>
2018-06-28 22:36:51 -0400

    Removed the header component. Spent more time learning the css grid system and incorporated a flexbox layout in with the navigation links.
    The header section is responsive now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Clark Brading | Portfolio';
+
+  width$: number;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.width$ = event.target.innerWidth;
+  }
+
+  ngOnInit() {
+    this.width$ = window.innerWidth;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { IdCardComponent } from './id-card/id-card.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SocialMediaComponent } from './social-media/social-media.component';
@@ -16,7 +15,6 @@ import { BackToTopComponent } from './back-to-top/back-to-top.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     IdCardComponent,
     NavigationComponent,
     SocialMediaComponent,
