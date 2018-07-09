e2aaa495a4e0bdb8bd30c0b4aa9f983897259f6a
Clark Brading <scbrading@gmail.com>
2018-07-08 23:24:51 -0400

    Expirmented with solutions to the problem where Safari mobile will not let the user tap the bottom 44px, without revealing the menu bar before taking any other actions.

--- src/app/back-to-top/back-to-top.component.ts
+++ src/app/back-to-top/back-to-top.component.ts
@@ -8,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class BackToTopComponent implements OnInit {
 
   // noinspection JSMethodCanBeStatic
-  protected scrollToTop() {
-    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  scrollToTop() {
+    // window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    // document.querySelector('div').scrollTo(0, 0);
+    // document.body.scrollTop = 0;
+    const scrollToTop = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 10); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 16);
+    console.log('run');
   }
 
   constructor() {
