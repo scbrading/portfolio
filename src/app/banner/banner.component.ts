863eaebda9deba7af2c95156e83f69a493e741d6
Clark Brading <scbrading@gmail.com>
2018-07-10 01:20:36 -0400

    Made changes to the API between the banner component and its parents. Instead of passing the filename directly to the banner component, the parent component name is passed. This allows the banner-component to identify itself which image to display.
    Since the banner-component knows all of the image files available for display, it loads all of them but displays only the appropriate image, identified by its parent.
    
    Changed the filename structure of the assets. Added jpg alternatives.

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,22 +1,88 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent implements OnInit {
+export class BannerComponent implements OnInit, OnChanges {
   @Input() text: string;
+  @Input() parent: string;
   @Input() imageFilename: string;
   @Input() imageQuality: string;
 
   backgroundStyle$: string;
 
+  routeToAssets$ = '../assets/';
+  bannerImages$: object[] = [
+    {
+      name: 'home',
+      quality: '3x',
+      type: 'png',
+      filename: 'from_idea_to_production_banner'
+    },
+    {
+      name: 'portfolio-overview',
+      quality: '3x',
+      type: 'png',
+      filename: 'tools_and_companies_banner'
+    },
+    {
+      name: 'es-borders',
+      quality: '3x',
+      type: 'png',
+      filename: '86borders_banner'
+    },
+    {
+      name: 'sesra',
+      quality: '3x',
+      type: 'png',
+      filename: 'sesra_banner'
+    },
+    {
+      name: 'special-projects',
+      quality: '3x',
+      type: 'png',
+      filename: 'special_projects_banner'
+    },
+    {
+      name: 'contact',
+      quality: '3x',
+      type: 'png',
+      filename: 'ideas_spring_out_of_head_banner'
+    },
+    {
+      name: 'about',
+      quality: '2x',
+      type: 'jpg',
+      filename: 'atl_falcon_at_stadium_banner-80'
+    }];
+
+  bannerImageUrls$: object[] = [{}];
+
+  constructBannerImageUrls(collectionArray: object[]) {
+    for (let i = 0; i < collectionArray.length; i++) {
+      const temporaryObject = {url: '', active: false};
+
+      temporaryObject.url = this.routeToAssets$ + collectionArray[i].quality + '/' + collectionArray[i].filename + '.' + collectionArray[i].type;
+
+      if (collectionArray[i].name === this.parent) {
+        temporaryObject.active = true;
+        this.backgroundStyle$ = 'url(' + temporaryObject.url + ')';
+      }
+      this.bannerImageUrls$[i] = temporaryObject;
+    }
+  }
+
   constructor() {
   }
 
+  ngOnChanges() {
+    this.constructBannerImageUrls(this.bannerImages$);
+  }
+
   ngOnInit() {
-    this.backgroundStyle$ = 'url("../../assets/' + this.imageQuality + '/' + this.imageFilename + '")';
+    this.constructBannerImageUrls(this.bannerImages$);
   }
 
 }
